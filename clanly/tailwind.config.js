22e51fbb67060c5707cb946722cf68896664b2e9
dferrero17 <danielmartinsferreira@ua.pt>
2022-12-28 20:45:10 +0000

    added tailwind and daisyUI config

---
+++ clanly/tailwind.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+
+
+
+          "primary": "#ffffff",
+
+
+
+          "secondary": "#98f785",
+
+
+
+          "accent": "#51a81f",
+
+
+
+          "neutral": "#1C1D22",
+
+
+
+          "base-100": "#4A1D96",
+
+
+
+          "info": "#6BC7DB",
+
+
+
+          "success": "#79ECE2",
+
+
+
+          "warning": "#F4B05D",
+
+
+
+          "error": "#EF6164",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+
+};
