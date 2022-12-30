9691089a198b5ba2bf2ee23354ffd48350d56a7f
dferrero17 <danielmartinsferreira@ua.pt>
2022-12-30 03:34:14 +0000

    added home page functionality + responsiveness

--- clanly/src/app/app.module.ts
+++ clanly/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     LoginComponent,
     HomeComponent,
     NavbarComponent,
+    PostComponent,
 
   ],
   imports: [
