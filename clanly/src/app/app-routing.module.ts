e811520e30be93dad33b333cd80eddaa176b9cc0
dferrero17 <danielmartinsferreira@ua.pt>
2023-01-02 23:00:14 +0000

    turururururu

--- clanly/src/app/app-routing.module.ts
+++ clanly/src/app/app-routing.module.ts
@@ -10,11 +10,13 @@ import { SignupComponent } from './signup/signup.component';
 import { CreateclanComponent } from './createclan/createclan.component';
 import { PostpageComponent } from './postpage/postpage.component';
 import { ClanComponent } from './clan/clan.component';
+import { EditprofileComponent } from './editprofile/editprofile.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'postpage', component: PostpageComponent},
   { path: 'user', component: UserComponent },
+  { path: 'editprofile', component: EditprofileComponent},
   { path: 'searchresults', component: SearchresultsComponent },
   { path: 'myclans', component: MyclansComponent },
   { path: 'clan', component: ClanComponent },
